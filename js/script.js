d6d9fd4f4002806a10d38d3bbdcc05ab069908aa
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-04-02 22:02:59 +1100

    create js script file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const openNav = () => {
+    if(document.getElementById("mobile-nav").style.width === ""){
+        document.getElementById("mobile-nav").style.width = "250px"
+    } else{
+        document.getElementById("mobile-nav").style.width = ""
+    }
+        
+
+  
+
+    
+}
