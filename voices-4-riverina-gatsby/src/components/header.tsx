6293b21ff18ccf217724313dd75e9c0baf05f2ee
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-05-05 15:33:29 +1000

    add header and header image, add assets

--- voices-4-riverina-gatsby/src/components/header.tsx
+++ voices-4-riverina-gatsby/src/components/header.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import {StaticImage} from "gatsby-plugin-image"
 
 
 type props = {
@@ -7,6 +8,31 @@ type props = {
 }
 const Header = ({ siteTitle}: props) => (
  <header>
+           <Link to="/" >
+    <StaticImage src="../assets/v4rlogopng.png" alt="Voices 4 Riverina" width={200}/>
+</Link>
+
+    <nav id="desktop-nav">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/our-story.html">Our Story</a></li>
+            <li><a href="/get-involved.html">Get Involved</a></li>
+            <li><a href="/contact.html">Contact</a></li>
+        </ul>
+       
+    
+</nav>
+<nav id="mobile-nav">
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/our-story.html">Our Story</a></li>
+            <li><a href="/get-involved.html">Get Involved</a></li>
+            <li><a href="/contact.html">Contact</a></li>
+    </ul>
+   
+
+</nav>
+<img src="assets/hamburger.svg" className="menu" onClick="openNav()"/>
 
  </header>
 )
